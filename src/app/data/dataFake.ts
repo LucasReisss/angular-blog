e053cfc0cd3c0bd59abd00563d16d93778dfeef2
Lucas Reis <lucasbarroreis@gmail.com>
2023-12-29 15:15:19 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Desvendando os Segredos do Multiverso Aracnídeo!",
+    "description": "Adentre o AranhaVerso: teias que transcendem dimensões, heróis inéditos e mistérios entrelaçados. Uma jornada épica além do imaginável!",
+    "photoCover": "https://www.cineset.com.br/wp-content/uploads/2023/06/Critica-Homem-Aranha-Atraves-do-Aranhaverso-2023-3.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Marvel: Emoções Cinematográficas!",
+    "description": "Exploração profunda das experiências emocionais proporcionadas pelo universo cinematográfico da Marvel. Neste blog, mergulhamos nas nuances das narrativas, personagens e momentos épicos que despertam uma variedade de emoções, desde a empolgação até a emoção intensa. Descubra como as sagas e heróis da Marvel capturam o coração dos espectadores, proporcionando uma jornada cinematográfica repleta de emoções cativantes e inesquecíveis.",
+    "photoCover": "https://www.showmetech.com.br/wp-content/uploads//2023/05/Imagem-destacada-1858x1024.jpg"
+  },
+  {
+    "id": "3",
+    "title": "X-Men: Mutantes em Destaque!",
+    "description": "Exploração profunda das experiências emocionais proporcionadas pelo universo cinematográfico da Marvel. Neste blog, mergulhamos nas nuances das narrativas, personagens e momentos épicos que despertam uma variedade de emoções, desde a empolgação até a emoção intensa. Descubra como as sagas e heróis da Marvel capturam o coração dos espectadores, proporcionando uma jornada cinematográfica repleta de emoções cativantes e inesquecíveis.",
+    "photoCover": "https://rollingstone.uol.com.br/media/_versions/painel-lona-festa-infantil-x-men-1-00x0-70-painel_1_widelg.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Homem de Ferro: Entre Engrenagens e Heroísmo",
+    "description": "Adentre o universo de \"Homem de Ferro: Entre Engrenagens e Heroísmo\", onde a genialidade de Tony Stark se entrelaça com a bravura do herói. Explore as intricadas engrenagens da tecnologia e testemunhe a jornada épica de um homem que se transforma em lenda.",
+    "photoCover": "https://images.universohq.com/2013/05/hdf3_tonystark_des.jpg"
+  },
+]
